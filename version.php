ae62269d4e309ae6b386dfb47adebc39a853c97e
Johannes <ichraum@gmail.com>
2026-04-06 20:32:02 +0200

    chore: set release to 0.9.0, maturity BETA, version 2026040601

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_eledialeitnerflow';
-$plugin->version   = 2024120124;
+$plugin->version   = 2026040601;
 $plugin->requires  = 2024042200; // Moodle 4.4+.
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.3.0';
+$plugin->maturity  = MATURITY_BETA;
+$plugin->release   = '0.9.0';
